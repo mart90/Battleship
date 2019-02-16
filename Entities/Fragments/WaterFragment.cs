97a40fa50311e54f82fff0e08024e8bd93bcab74
Mart Broekkamp <m.broekkamp@zetacom.nl>
2019-02-16 16:27:56 +0100

    .

--- Entities/Fragments/WaterFragment.cs
+++ Entities/Fragments/WaterFragment.cs
@@ -11,7 +11,21 @@ namespace MBRD.Entities
 
         public void Draw(ContentManager contentManager, SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(contentManager.Load<Texture2D>("Sprites/WaterTiles/48"), Location, Color.White);
+            Rectangle rect = new Rectangle(0, 64, 32, 32);
+            Vector2 pos = new Vector2(100, 100);
+            Vector2 origin = new Vector2(0, 0);
+
+            spriteBatch.Draw(contentManager.Load<Texture2D>("icon-sprite"), 
+                pos, 
+                rect, 
+                Color.Wheat, 
+                0.0f,
+                origin, 
+                1, 
+                SpriteEffects.None, 
+                0.0f);
+
+            //spriteBatch.Draw(contentManager.Load<Texture2D>("Sprites/WaterTiles/48"), Location, Color.White);
         }
     }
 }
