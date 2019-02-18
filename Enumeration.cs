2972711b9b56378d2e52210779924ad1234a1501
Frank Pons <f.pons@zetacom.nl>
2019-02-18 16:27:13 +0100

    Fixes #5. It is now moved to Enumerations file

---
+++ Enumeration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBRD
+{
+    enum GameState
+    {
+        StartMenu,
+        Loading,
+        Playing,
+        Paused
+    }
+}
